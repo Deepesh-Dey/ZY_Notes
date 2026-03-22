71808378bcdac6911795749dbedae742eff2e877
Deepesh-Dey <newdeepesh24@gmail.com>
2026-03-22 12:52:32 +0530

    Setting up Routes

--- backEnd/server.js
+++ backEnd/server.js
@@ -4,12 +4,17 @@ import express from "express";
 const app = express();
 
 app.get("/api/notes", (req, res) => {
-    res.status(200).send("You got 5 Notes");
+    res.status(200).send("You got 10 Notes");
 });
 app.post("/api/notes", (req, res) => {
-    res.status(201).send("Your Note is created successfully");
+    res.status(201).json({ message: "Note is created successfully" });
 });
-
+app.put("/api/notes/:ID", (req, res) => { 
+    res.status(201).json({ message: "Note is updated successfully" });
+});
+app.delete("/api/notes/:ID", (req, res) => {
+    res.status(200).json({ message: "Note is deleted successfully" });
+});
 app.listen(5001, () => {
     console.log("Server started on port: 5001");
 })
